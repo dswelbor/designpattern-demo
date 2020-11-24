831b19563e7cf89e9fe247595b285797f1810130
jlabrec <jacob.labrec@gmail.com>
2020-11-24 01:26:07 -0600

    Adds runAdapter() method for adapter demo

--- java/src/main/java/Main.java
+++ java/src/main/java/Main.java
@@ -1,7 +1,15 @@
 import adapter.*;
-public class Main {
 
-    private static void adapterDemo() {
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Main {
+    // List of valid args
+    private static List<String> allArgs = Arrays.asList("-adapter");
+
+
+    private static void runAdapter() {
         System.out.println("You've got an Iphone 5 and wanna listen to music with your 3.5 mm headphones. Easy");
         Headphones headphones = new Headphones();
         headphones.listen();
@@ -13,11 +21,46 @@ public class Main {
 
         System.out.println("Luckily, we know we can just use an adapter, to adapt to this new interface");
         headphones.iphone = new USBAdapter();
+        System.out.println("\n\nAdapter applied, lets listen to music:");
         headphones.listen();
     }
 
-    public static void main(String[] args){
-        adapterDemo();
+    private static boolean validateArgs(String[] pArgs) {
+        // Validate args
+        StringBuilder badArgs = new StringBuilder();
+        for (String arg: pArgs) {
+            if (!allArgs.contains(arg)) {
+                badArgs.append(" ");
+                badArgs.append(arg);
+            }
+
+        }
+        // invalid args
+        if (badArgs.length() > 0) {
+            System.out.printf("Unknown command-line option(s):%s\n", badArgs.toString());
+            // TODO: Add help text that informs usage of cli args
+
+            System.exit(0);
+        }
+        return true;
+    }
+
+    public static void main(String[] pArgs) {
+        // TODO: Refactor as abstracted cli runtime config
+        List<String> cliArgs = new LinkedList<>(Arrays.asList(pArgs));
+        validateArgs(pArgs);
+
+        // Default - no params, run all
+        if (cliArgs.isEmpty()) {
+
+            cliArgs.addAll(allArgs);
+        }
+
+        // execute examples
+        if (cliArgs.contains("-adapter")){
+            runAdapter();
+        }
+
     }
 
 }
